665ff8cccbd749a4e800a528ff395f19422b19ff
Jackyzhu6 <ibm20023@gmail.com>
2017-06-03 05:20:28 -0400

    Update global.r

--- 79/global.r
+++ 79/global.r
@@ -1,1 +1,16 @@
 options(shiny.usecairo = FALSE)
+
+font_home <- function(path = '') file.path('~', '.fonts', path)
+if (Sys.info()[['sysname']] == 'Linux' &&
+      system('locate wqy-zenhei.ttc') != 0 &&
+      !file.exists(font_home('wqy-zenhei.ttc'))) {
+  if (!file.exists('wqy-zenhei.ttc'))
+    curl::curl_download(
+      'https://github.com/rstudio/shiny-examples/releases/download/v0.10.1/wqy-zenhei.ttc',
+      'wqy-zenhei.ttc'
+    )
+  dir.create(font_home())
+  file.copy('wqy-zenhei.ttc', font_home())
+  system2('fc-cache', paste('-f', font_home()))
+}
+rm(font_home)
