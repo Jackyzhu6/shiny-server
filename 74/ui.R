e13403dceb92c0d656baa71a30930ea4bb48f31d
Jackyzhu6 <ibm20023@gmail.com>
2017-06-09 22:51:06 -0400

    Add files via upload

--- 74/ui.R
+++ 74/ui.R
@@ -24,6 +24,7 @@ shinyUI(fluidPage(
                             }
                             "))),
   tags$head(includeScript("google-analytics.js")),
+  headerPanel(windowTitle = "74战场数据库",title = ""),
   div(
     h1("七绝仙魔阵，四海夜狼声 - 74战场数据库"),
     p(a("天下3论坛",href = "http://tx3.netease.com/",stype = "color:red;",target = "_blank"),
