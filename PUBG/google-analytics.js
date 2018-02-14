58d3dd931015943959952347fd3fb656b51134b0
Jackyzhu6 <ibm20023@gmail.com>
2018-02-14 17:55:16 -0500

    Update google-analytics.js

--- PUBG/google-analytics.js
+++ PUBG/google-analytics.js
@@ -3,7 +3,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-99134935-1', 'auto');
+  ga('create', 'UA-99134935-2', 'auto');
   ga('send', 'pageview');
   $(document).on('shiny:inputchanged', function(event) {
        ga('send','event', 'input', 'updates',event.value);
